1086197d478907d4616f6615e8f75f4124082bc4
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-19 11:52:49 +0000

    Created the game loop in Application

--- src/main/java/spacewars/Application.java
+++ src/main/java/spacewars/Application.java
@@ -13,4 +13,26 @@ public class Application {
             e.printStackTrace();
         }
     }
+
+    private void run() throws IOException {
+        int FPS = 60;
+        int frameTime = 1000 / FPS;
+
+        while (true) {
+            long startTime = System.currentTimeMillis();
+
+            // Update game state
+            // Render game state
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+
+            try {
+                if (sleepTime > 0) Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+            }
+
+        }
+        //close game (gui probably)
+    }
 }
