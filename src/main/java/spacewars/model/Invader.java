095d47ff3c5b3d81b1acbf03016909559cda1988
João Lopes <158082419+joaolopes15@users.noreply.github.com>
2024-11-20 18:44:52 +0000

    abstract class invader created

---
+++ src/main/java/spacewars/model/Invader.java
@@ -0,0 +1,11 @@
+package spacewars.model;
+
+public abstract class Invader extends Entity {
+    public Invader(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void update() {
+    }
+}
