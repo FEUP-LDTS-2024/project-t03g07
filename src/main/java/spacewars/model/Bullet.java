1b5e588823cb2a386f0168835e09cfccd785fd60
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-22 12:05:56 +0000

    Created the class Bullet

---
+++ src/main/java/spacewars/model/Bullet.java
@@ -0,0 +1,32 @@
+package spacewars.model;
+
+public class Bullet
+{
+    private Position position;
+    private int speed;
+
+    public Bullet(int x, int y, int speed)
+    {
+        this.position = new Position(x, y);
+        this.speed = speed;
+    }
+
+    public Position getPosition()
+    {
+        return this.position;
+    }
+    public void setPosition(Position position)
+    {
+        this.position = position;
+    }
+
+
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+    public void setSpeed(int speed)
+    {
+        this.speed = speed;
+    }
+}
