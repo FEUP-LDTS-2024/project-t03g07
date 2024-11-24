db3f1e89681e2ad77ba75ff58e43b17cc5a2f793
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-24 13:02:21 +0000

    Created tests for the methods in Score

---
+++ src/test/java/ScoreTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import spacewars.model.game.HighScore;
+import spacewars.model.game.Score;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoreTest
+{
+    @Test
+    public void testIncreaseScore()
+    {
+        Score score = new Score();
+        score.increaseScore(10);
+        assertEquals(10, score.getScore());
+    }
+
+    @Test
+    public void testResetScore()
+    {
+        Score score = new Score();
+        score.increaseScore(10);
+        score.resetScore();
+        assertEquals(0, score.getScore());
+    }
+
+    @Test
+    public void testCheckScoreNotHighScore() {
+        Score score = new Score();
+        score.increaseScore(5);
+        HighScore.saveHighScore(10);
+        score.checkScore();
+        assertEquals(5, score.getScore());
+    }
+
+    @Test
+    public void testCheckScoreHighScore() {
+        Score score = new Score();
+        score.increaseScore(15);
+        HighScore.saveHighScore(10);
+        score.checkScore();
+        assertEquals(15, score.getScore());
+    }
+
+}
