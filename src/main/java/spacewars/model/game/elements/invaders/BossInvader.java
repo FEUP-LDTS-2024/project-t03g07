e2bf68fc8110bbf5eebde4bd0f9bf741c78c9d31
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-24 16:29:03 +0000

    Made it so the points have different probability

--- src/main/java/spacewars/model/game/elements/invaders/BossInvader.java
+++ src/main/java/spacewars/model/game/elements/invaders/BossInvader.java
@@ -7,10 +7,26 @@ public class BossInvader extends Invader{
 
     public BossInvader(int x, int y) {
         super(x, y);
-        int[] possiblePoints = {75, 100, 250, 500, 999};
-        Random random = new Random();
-        this.points = possiblePoints[random.nextInt(possiblePoints.length)];
+        this.points = getRandomPoints();
     }
+
+
+    private int getRandomPoints() {
+        int[] possiblePoints = {75, 100, 250, 500, 999};
+        double[] probabilities = {0.5, 0.2, 0.15, 0.1, 0.05}; //probabilities must sum to 1
+
+        double randomValue = new Random().nextDouble();
+        double cumulativeProbability = 0.0;
+
+        for (int i = 0; i < possiblePoints.length; i++) {
+            cumulativeProbability += probabilities[i];
+            if (randomValue <= cumulativeProbability) {
+                return possiblePoints[i];
+            }
+        }
+        return possiblePoints[possiblePoints.length - 1]; //fallback in case of rounding errors
+    }
+
 
     @Override
     public int getPoints() {
