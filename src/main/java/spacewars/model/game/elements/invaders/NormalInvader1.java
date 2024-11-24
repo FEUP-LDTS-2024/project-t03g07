5b4aeeaa3d5217e0a29a3a8c49ae51505254703d
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-24 14:35:33 +0000

    Added a new method getPoints to return the points that each invader is worth.

--- src/main/java/spacewars/model/game/elements/invaders/NormalInvader1.java
+++ src/main/java/spacewars/model/game/elements/invaders/NormalInvader1.java
@@ -2,8 +2,16 @@ package spacewars.model.game.elements.invaders;
 
 public class NormalInvader1 extends Invader
 {
+    private final int points = 10;
+
     public NormalInvader1(int x, int y)
     {
         super(x, y);
+    }
+
+    @Override
+    public int getPoints()
+    {
+        return points;
     }
 }
