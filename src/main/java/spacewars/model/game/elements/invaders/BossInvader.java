0122fa43d4eeaa3d455704ce98890feb46907aaa
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-11-24 16:26:43 +0000

    Made it so that the points of the boss invader are random

--- src/main/java/spacewars/model/game/elements/invaders/BossInvader.java
+++ src/main/java/spacewars/model/game/elements/invaders/BossInvader.java
@@ -1,10 +1,15 @@
 package spacewars.model.game.elements.invaders;
 
+import java.util.Random;
+
 public class BossInvader extends Invader{
-    private final int points = 100;
+    private final int points;
 
     public BossInvader(int x, int y) {
         super(x, y);
+        int[] possiblePoints = {75, 100, 250, 500, 999};
+        Random random = new Random();
+        this.points = possiblePoints[random.nextInt(possiblePoints.length)];
     }
 
     @Override
