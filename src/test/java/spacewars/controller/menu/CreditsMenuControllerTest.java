f87f177a967197aa0d0e82d19dd8c8795e9c47ca
João Lopes <158082419+joaolopes15@users.noreply.github.com>
2024-12-22 15:44:25 +0000

    implemented tests for Creditsmenucontroller

--- src/test/java/spacewars/controller/menu/CreditsMenuControllerTest.java
+++ src/test/java/spacewars/controller/menu/CreditsMenuControllerTest.java
@@ -1,4 +1,41 @@
 package spacewars.controller.menu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spacewars.Application;
+import spacewars.model.menu.CreditsMenu;
+import spacewars.states.MainMenuState;
+import spacewars.view.images.ImageLoader;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.mockito.Mockito.*;
+
 public class CreditsMenuControllerTest {
+    private CreditsMenuController controller;
+    private CreditsMenu creditsMenu;
+    private Application application;
+    private EntryController entryController;
+    private ImageLoader imageLoader;
+
+    @BeforeEach
+    public void setUp() {
+        creditsMenu = mock(CreditsMenu.class);
+        application = mock(Application.class);
+        entryController = mock(EntryController.class);
+        imageLoader = mock(ImageLoader.class);
+        controller = new CreditsMenuController(creditsMenu, entryController);
+
+        when(application.getImageLoader()).thenReturn(imageLoader);
+    }
+
+    @Test
+    public void testOnQuit() throws IOException, FontFormatException, URISyntaxException {
+        controller.onQuit(application);
+
+        // Verify that the application state is set to MainMenuState
+        verify(application, times(1)).setState(isA(MainMenuState.class));
+    }
 }
