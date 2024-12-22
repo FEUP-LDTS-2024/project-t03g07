bc674615b61d0f2f6ef89bb789938b751549492b
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2024-12-22 16:44:59 +0000

    Created the BulletInvader1Test class to test the methods in class BulletInvader1 and also changed the BulletPlayerTest methods to have a 'Property' annotation

--- src/test/java/spacewars/model/game/elements/bullets/BulletInvader1Test.java
+++ src/test/java/spacewars/model/game/elements/bullets/BulletInvader1Test.java
@@ -5,22 +5,21 @@ import net.jqwik.api.Property;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BulletPlayerTest {
+public class BulletInvader1Test {
 
     @Property
-    public void testBulletPlayerInitialization(@ForAll double x, @ForAll double y) {
-        BulletPlayer bullet = new BulletPlayer(x, y);
+    public void testBulletInvader1Initialization(@ForAll double x, @ForAll double y) {
+        BulletInvader1 bullet = new BulletInvader1(x, y);
         assertEquals(x, bullet.getPosition().x());
         assertEquals(y, bullet.getPosition().y());
-        assertEquals(5, bullet.getSpeed());
+        assertEquals(1, bullet.getSpeed());
     }
 
     @Property
     public void testBulletPlayerUpdate(@ForAll double x, @ForAll double y) {
-        BulletPlayer bullet = new BulletPlayer(x, y);
+        BulletInvader1 bullet = new BulletInvader1(x, y);
         bullet.update();
         assertEquals(x, bullet.getPosition().x());
-        assertEquals(y - bullet.getSpeed(), bullet.getPosition().y());
+        assertEquals(y + bullet.getSpeed(), bullet.getPosition().y());
     }
-
 }
